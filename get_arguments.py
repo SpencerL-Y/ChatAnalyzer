1e057b18056609dea325c59d6d7fddeb93727e70
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-05-21 17:53:20 +0800

    openai version update

--- get_arguments.py
+++ get_arguments.py
@@ -13,6 +13,20 @@ def get_filename_firstline(function_name):
             print(final)
             return ["./linux/" + items[1], final]
     return ["NOT FOUND", "NOT_FOUND"]
-        
+
+def get_funcname_firstline_linux_folder(function_name, linux_folder):
+    f = open(linux_folder + "/tags")
+    path = os.system("pwd")
+    for line in f.readlines():
+        items = line.split("\t")
+        # print(items)
+        if(items[0] == function_name):
+            latter = items[2][2:]
+            final = latter[:-4]
+            print(linux_folder + items[1])
+            print(final)
+            return [linux_folder + items[1], final]
+    return ["NOT FOUND", "NOT_FOUND"]
+
 if __name__ == "__main__":
     get_filename_firstline()
